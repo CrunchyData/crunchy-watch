cda7b761e9261c1ef2813ee86f6689c4acd77cbc
Jeff McCormick <jeff.mccormick@crunchydata.com>
2017-02-01 16:20:55 -0600

    initial

--- plugins/dockerplugin.go
+++ plugins/dockerplugin.go
@@ -18,6 +18,7 @@ package plugins
 import (
 	api "github.com/crunchydata/crunchy-watch/watchapi"
 	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/filters"
 	"github.com/docker/docker/client"
 	"golang.org/x/net/context"
 )
@@ -32,13 +33,41 @@ func DockerFailover() {
 		panic(err)
 	}
 
-	containers, err := cli.ContainerList(context.Background(), types.ContainerListOptions{})
+	filters := filters.NewArgs()
+	//filters.Add("label", "label1")
+	//filters.Add("label", "label2")
+	filters.Add("name", "/"+api.EnvVars.PG_SLAVE_SERVICE)
+	containers, err := cli.ContainerList(context.Background(), types.ContainerListOptions{
+		Size:    true,
+		All:     true,
+		Filters: filters})
 	if err != nil {
-		panic(err)
+		api.Logger.Println(err.Error())
+		return
 	}
+
+	startConfig := types.ExecStartCheck{}
+	execConfig := types.ExecConfig{}
+
+	x := []string{"touch", "/tmp/foo"}
+	execConfig.Cmd = x
+	execConfig.AttachStdout = true
 
 	for _, container := range containers {
 		api.Logger.Printf("%s %s\n", container.ID[:10], container.Image)
+		api.Logger.Println("replica host name is " + container.Names[0])
+
+		id, err2 := cli.ContainerExecCreate(context.Background(), api.EnvVars.PG_SLAVE_SERVICE, execConfig)
+		if err2 != nil {
+			api.Logger.Println("error on exec create " + err2.Error())
+			return
+		}
+		api.Logger.Println("exec id is " + id.ID)
+		err2 = cli.ContainerExecStart(context.Background(), id.ID, startConfig)
+		if err2 != nil {
+			api.Logger.Println("error on exec start " + err2.Error())
+			return
+		}
 	}
 	api.Logger.Println("docker failover ends....")
 }
