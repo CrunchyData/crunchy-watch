27b29bac9eefa3195d4d8e9b663c30c9dbab1a04
Jeff McCormick <jeff.mccormick@crunchydata.com>
2017-02-01 16:20:41 -0500

    initial

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+export PROJECT_TYPE=docker
+export PG_MASTER_SERVICE=master
+export PG_SLAVE_SERVICE=replica
+export PG_MASTER_PORT=5432
+export PG_MASTER_USER=master
+export PG_DATABASE=postgres
+export SLAVE_TO_TRIGGER_LABEL=
+export SLEEP_TIME=10
+export WAIT_TIME=50
+export DOCKER_API_VERSION=1.12
+go run watchserver.go
